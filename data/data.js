21dfc1520fda5c8a736ff49f7f61ae0d435a2d42
you-seang <ysangstar@gmail.com>
2020-09-26 22:03:48 +0900

    기본 포트폴리오 완성

--- data/data.js
+++ data/data.js
@@ -6,76 +6,62 @@
 
 // Change your profile data here
 let profile = {
-  picture: "",
-  name: "유 세앙",
-  description: "Software Engineer & Fullstack Developer",
+  picture: "img/profile.png",
+  name: "You Seang",
+  description: "Ready banker",
   bio:
     "I'm a Software Engineer interested in Internet technologies, computer networks and security. From the FullStack development to the most innovative technologies such as Blockchain.<br><br>High level of web languages (JS, HTML and CSS), average level of C and C++. Great understanding of Internet protocols, P2P networks and cloud infrastructures. I work with JS frameworks, JQuery (old days) and React.js (now).",
-  email: "guim.gonzalez@gmail.com",
-  location: "Barcelona, Spain",
-  birth: "1997-07-26", // YYYY-MM-DD format
+  email: "ysangstar@gmail.com",
+  location: "Gwanak-gu, Seoul",
+  birth: "1993-09-10", // YYYY-MM-DD format
   skills: [
-    "Blockchain",
-    "Bitcoin",
-    "C/C++",
-    "CSS",
-    "Ethereum",
-    "HTML",
-    "Javascript",
-    "MongoDB",
-    "NodeJS",
-    "React",
-    "Solidity",
-    "SQL",
-    "Web3"
+    "Certified Investment Manager (투자자산운용사)",
+    "Certified Foreign Exchange Specialist (외환전문역 1종)",
   ],
   social: {
-    linkedin: "https://www.linkedin.com/in/guim-gonzalez-846606134/",
-    twitter: "https://twitter.com/GuimGonzalez",
-    github: "https://github.com/GuimG/",
-    stackoverflow: "https://stackoverflow.com/users/8700961/guim-gonzalez",
-    dev: "https://dev.to/guimg"
+    linkedin: "",
+    twitter: "",
+    stackoverflow: "",
+    dev: "https://www.notion.so/Economy-ce62356052344f57b305ad61f706f294",
   },
   experience: [
     {
-      title: "FullStack Developer",
-      company: "Freelance",
+      title: "ROK Army Officer",
+      company: "Republic of Korea army (ROKA), Lieutenant expired discharge",
       url: "https://guimgonzalez.business/",
-      location: "Barcelona, Spain",
-      description:
-        "I do websistes for small businesses. I develop both frontend and backend. I also setup and mantain the VPS.",
-      from: "September 2017",
-      to: "Now"
+      location: "Republic of Korea (ROK)",
+      description: "Personnel Manager, Headquarters Company Commander",
+      from: "2016.03.01",
+      to: "2018.06.29",
     },
     {
-      title: "IT Consultant",
-      company: "Innova-TSN",
+      title: "Korea Communications Standards Commission",
+      company: "Korea Communications Standards Commission",
       url: "https://www.innova-tsn.com/",
-      location: "Barcelona, Spain",
-      description:
-        "I developed web services, worked with SAS (ETL's), and email marketing with Oracle Eloqua.",
-      from: "July 2018",
-      to: "February 2019"
-    }
+      location: "Yangcheon-gu, Seoul",
+      description: "Professional monitoring personnel",
+      from: "2019.02",
+      to: "Now",
+    },
   ],
   education: [
     {
-      school: "Universitat Autònoma de Barcelona",
-      degree: "Engineering",
-      fieldOfStudy: "Computer Science",
-      location: "Barcelona, Spain",
-      from: "September 2016",
-      to: "Now"
-    }
+      school: "Kyunggi University",
+      degree: "major of Law, doble major of Intellectual Property Studies",
+      fieldOfStudy: "",
+      location: "Suwon, Kyunggi-Do",
+      from: "2012.03",
+      to: "2016.02",
+    },
   ],
-  quote: "I turn coffee into code"
+  quote: "Customer's happiness is my happiness",
 };
 
 let page = {
   language: "english",
   // ^ languages suported:
   //  - english (default)
-  theme: "nightsky"
+  theme: "purpledream",
   // ^ themes suported:
   //  - nightsky (default)
   //  - sunset
